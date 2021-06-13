2510e15415c58d9fa53821aea56eaae0b7bf4f9b
hari-hech-kay <48200258+hari-hech-kay@users.noreply.github.com>
2021-06-13 18:05:10 +0530

    Initial commit

---
+++ src/main/java/com/example/SpringReddit/SpringRedditApplication.java
@@ -0,0 +1,15 @@
+package com.example.SpringReddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class SpringRedditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedditApplication.class, args);
+	}
+
+}
